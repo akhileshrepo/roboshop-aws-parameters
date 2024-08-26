615f150783855e83b3f9e952a761a4c67f65218f
AKhilesh <kothapallyakhil123@gmail.com>
2024-08-26 22:34:55 +0530

    changes

--- main.tf
+++ main.tf
@@ -3,5 +3,6 @@ resource "aws_ssm_parameter" "main" {
   name  = each.key
   type  = each.value["type"]
   value = each.value["value"]
+  key_id = "arn:aws:kms:us-east-1:058264419835:key/935b4d54-667e-4ab5-903d-55c42a81b9a0"
 }
 
